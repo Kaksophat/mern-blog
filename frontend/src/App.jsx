bc2f20d497d09e2f6dc6462fd2c1e431c00d7341
kak sophat <kaksophatt@gmail.com>
2024-07-25 17:17:55 +0700

    create route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 
-
+import { Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
+import Project from "./pages/Project"
 function App() {
   
 
   return (
     <>
-     <h1 className="text-slate-600">hello</h1>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/project" element={<Project/>}/>
+     </Routes>
     </>
   )
 }
